0d60ebee67519c7bab558cf159a4341055379d17
Ankit <ankitkrb4644@gmail.com>
2023-10-12 22:57:17 +0530

    update

--- for_loop.java
+++ for_loop.java
@@ -1,3 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class for_loop {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=1;i<=10;i++){
+        int Result=N*i;
+            System.out.println(N+" x " +i+ " = " +Result);
+        }
+        bufferedReader.close();
+    }
 }
+
