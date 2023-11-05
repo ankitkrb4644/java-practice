65dbdb4b7b0b7b26badefe9d6fb1cc1d0ac61d1b
Ankit <ankitkrb4644@gmail.com>
2023-11-05 20:48:51 +0530

    update

--- multi_dim_arr.java
+++ multi_dim_arr.java
@@ -1,7 +1,8 @@
 public class multi_dim_arr {
     public static void main(String[] args){
-        int [][] flats;
-        flats = new int[2][3];
+//        int [][] flats;
+//        flats = new int[2][3];
+        int [][] flats = new int[2][3];
         flats[0][0]=101;
         flats[0][1]=102;
         flats[0][2]=103;
---
+++ practice_set_arr.java
@@ -0,0 +1,5 @@
+public class practice_set_arr {
+     public class static void main(String[] args){
+
+    }
+}
