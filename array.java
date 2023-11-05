bc19233a9410e703a08642f2f19b635986f23072
Ankit <ankitkrb4644@gmail.com>
2023-11-05 11:31:37 +0530

    update

--- array.java
+++ array.java
@@ -1,3 +1,21 @@
 public class array {
-    
+    public static void main(String[] args){
+        // there are 3 main ways to create an array in java
+        //1.Declaration and memory allocation
+        // int [] marks = new int [5];
+
+        //2.Declaration and then memory allocation
+        // int [] marks;
+        // marks = new int[5];
+        // marks[0]= 100;
+        // marks[1]= 60;
+        // marks[2]= 70;
+        // marks[3]= 90;
+        // marks[4]= 86;
+
+
+        // 3.Declaration,memory allocation and intialization together
+        int [] marks={98,45,79,99,89};
+        System.out.println(marks[4]);
+    }
 }
