5cadb5c730d096788315f0656d276da8c3aad263
Ankit <ankitkrb4644@gmail.com>
2023-11-09 02:52:53 +0530

    update

--- method_overloading.java
+++ method_overloading.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class method_overloading {
+public class method_overloading {
+    static void foo(){
+        System.out.println("Good morning bro!");
+    }
+    static void foo (int a){
+        System.out.println("Good morning " + a + " bro!");
+    }
+//    static void change(int a){
+//        a=98;
+//    }
+//    static void change2(int [] arr){
+//        arr[0]=98;
+//    }
+//    static void tellJoke(){
+//        System.out.println("I invested a new word!\n" + "Plagraism!");
+//    }
+//
+//         tellJoke();
+//        case 1: Changing the integer
+//                int x=45;
+//        change(x);
+//        System.out.println("the value of x after running change is:" + x);
+//        case 1: changing the array
+//        int [] marks={52,73,77,89,98,94};
+//        change2(marks);
+//        System.out.println("The value of x after running change is:" + marks);
+public static void main(String[] args){
+    foo();
+    foo(30);
+    }
+
 }
