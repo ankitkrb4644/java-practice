a3d0620db2dcdc64e510d74a91160f981b5a5e4d
Ankit <ankitkrb4644@gmail.com>
2023-11-09 01:25:56 +0530

    update

--- methods.java
+++ methods.java
@@ -1,8 +1,27 @@
 public class methods {
+    static int logic(int x, int y){
+        int z;
+        if (x>y){
+            z=x+y;
+        }
+        else{
+            z=(x+y)*5;
+        }
+        return z;
+    }
     public static void main(String[] args){
         int a = 5;
         int b=7;
-        int c=(a+b)*5;
+        int c;
+//method invocation using Object creation
+// methods obj =new methods();
+//c=obj.logic(a,b);
+        c=logic(a,b);
+        int a1=34;
+        int b1=6;
+        int c1;
+        c1=logic(a1,b1);
         System.out.println(c);
+        System.out.println(c1);
     }
 }
