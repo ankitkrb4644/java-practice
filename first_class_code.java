785d1e36b7ad503c9489a24175a6cf70f9fd3afe
Ankit <ankitkrb4644@gmail.com>
2023-11-09 20:16:28 +0530

    update

--- first_class_code.java
+++ first_class_code.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class first_class_code {
+import java.sql.SQLOutput;
+
+class Employee{
+    int id;
+    int salary;
+    String name;
+    public void printDetails(){
+        System.out.println("My id is " + id);
+        System.out.println("and my name is " + name);
+    }
+    public int getSalary(){
+        return salary;
+    }
+}
+public class first_class_code {
+    public static void main(String[] args) {
+        System.out.println("This is our custom class");
+        Employee Ankit = new Employee();//instantiating a new Employee object
+        //setting Attributes
+        Ankit.id=07;
+        Ankit.salary=34;
+        Ankit.name ="Ankit_Barnwal";
+        //printing the Attributes
+//        System.out.println(Ankit.id);
+//        System.out.println(Ankit.name);
+        Ankit.printDetails();
+        int salary = Ankit.getSalary();
+        System.out.println(salary);
+    }
 }
--- oops_practice.java
+++ oops_practice.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class oops_practice {
+class employee{
+    int salary;
+    String name;
+
+    public int getSalary(){
+        return salary;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name =n;
+    }
+}
+public class oops_practice {
+    public static void main (String[] agrs ){
+    employee ankit = new employee();
+    ankit.setName("barnwal");
+    ankit.salary=700;
+    System.out.println(ankit.getSalary());
+    System.out.println( ankit.getName());
+    }
 }
