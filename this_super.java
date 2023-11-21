055519b3ecc0f2a596795f6806d51cda4b4863ae
Ankit <ankitkrb4644@gmail.com>
2023-11-22 01:27:25 +0530

    update

--- this_super.java
+++ this_super.java
@@ -1,18 +1,25 @@
-class ekclass{
+class second_Class{
     int a;
     public int getA(){
         return a;
     }
-    ekclass(int a) {
+    second_Class(int a) { //constructor
     this.a = a;
     }
     public int returnone(){
         return 1;
     }
 }
+class DoClass extends second_Class{
+  DoClass(int c){
+      super(c);
+      System.out.println("i'm a constructor");
+  }
+}
 public class this_super {
     public static void main(String[] args) {
-        ekclass e =new ekclass(4);
+        second_Class e =new second_Class(4);
         System.out.println(e.getA());
+        DoClass d = new DoClass(5);
     }
 }
