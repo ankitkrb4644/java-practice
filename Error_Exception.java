525b10e666cd10848bdb73328436bd127d17f113
Ankit <ankitkrb4644@gmail.com>
2023-11-23 01:10:23 +0530

    update

--- Error_Exception.java
+++ Error_Exception.java
@@ -1,2 +1,8 @@
 public class Error_Exception {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10/0; // it will give run time error
+//        System.out.println(a+b); //it will give syntax error
+        System.out.println(a);
+    }
 }
