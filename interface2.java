4025b799ed5745b020b688704c55db9c548ad464
Ankit <ankitkrb4644@gmail.com>
2023-11-22 23:55:28 +0530

    update

--- interface2.java
+++ interface2.java
@@ -1,12 +1,13 @@
 interface Bicycle{
-    void applyBrakes(int decrement);
+    int a=45;
+    void applyBrake(int decrement);
     void speedup(int increment);
 }
-class avoncylcle implements Bicycle{
-    void blowhorn(){
+class AvonCycle implements Bicycle{
+    void BlowHorn(){
         System.out.println("pee pee peee.....");
     }
-    public void applyBrakes(int decrement){
+    public void applyBrake(int decrement){
         System.out.println("applying break");
     }
     public void speedup(int increment){
@@ -16,6 +17,11 @@ class avoncylcle implements Bicycle{
 
 public class interface2 {
     public static void main(String[] args) {
-
+        AvonCycle cycleAnkit = new AvonCycle();
+        //you can create properties in interfaces
+        cycleAnkit.applyBrake(4);
+        //you cannot modify the properties in interfaces as they are final
+//        cycleAnkit.a= 454;
+        System.out.println(cycleAnkit.a);
     }
 }
