c8ed23a4169b17d8ac84b8c19758fae6516b94e1
Ankit <ankitkrb4644@gmail.com>
2023-11-22 11:54:38 +0530

    update

--- practice_inheritance.java
+++ practice_inheritance.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class practice_inheritance {
+class circle{
+    public int radius;
+    circle(int r){    //constructor
+        System.out.println("i'm circle parameterised constructor");
+        this.radius=r;
+    }
+    public double area(){
+        return Math.PI*this.radius*this.radius;
+        }
+}
+class cylinder1 extends circle{  // inheritance
+    public int height;
+    cylinder1(int r,int h){
+        super(r);   //reference of super class
+        System.out.println("I am cylinder1 parameter constructor");
+        this.height=h;
+    }
+    public double volume(){
+        return Math.PI*this.radius*this.radius*this.height;
+    }
+        }
+public class practice_inheritance {
+    public static void main(String[] args) {
+        //problem 1
+//        circle obj = new circle(12);
+        cylinder1 obj1 = new cylinder1(12,4);
+    }
 }
