9c22b349233a5a937bbd9dea28f830aa5b7e74db
Ankit <ankitkrb4644@gmail.com>
2023-11-22 14:11:09 +0530

    update

--- Abstract_class_Abstract_meth.java
+++ Abstract_class_Abstract_meth.java
@@ -1,3 +1,24 @@
+abstract class Parent2{
+      Parent2(){
+          System.out.println("i'm constructor of Base2");
+    }
+    public void sayHello(){
+          System.out.println("hello");
+    }
+    abstract public void greet();
+}
+class Child2 extends Parent2{
+    public void greet(){
+     System.out.println("Good morning");
+ }
+}
+abstract class Child3 extends Parent2{
+    public void th(){
+        System.out.println(" i am good");
+    }
+}
 public class Abstract_class_Abstract_meth {
-    
+    public static void main(String[] args) {
+
+    }
 }
