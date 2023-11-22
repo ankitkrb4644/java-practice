ced1a6e9f40054e7ac4db19034d1ae6aa8de2dac
Ankit <ankitkrb4644@gmail.com>
2023-11-22 22:55:44 +0530

    update

--- interface1.java
+++ interface1.java
@@ -6,12 +6,13 @@ interface Herbivore{
 
 }
 class Horse implements Animal,Herbivore{   //multi level inheritance
-    public void walk(){
+    public void walk(){   // you have to make it as a public
         System.out.println("walks on 4 legs");
     }
 }
 public class interface1 {
     public static void main(String[] args) {
         Horse horse = new Horse();
+        horse.walk();
     }
 }
--- interface2.java
+++ interface2.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class interface2 {
+interface Bicycle{
+    void applyBrakes(int decrement);
+    void speedup(int increment);
+}
+class avoncylcle implements Bicycle{
+    void blowhorn(){
+        System.out.println("pee pee peee.....");
+    }
+    public void applyBrakes(int decrement){
+        System.out.println("applying break");
+    }
+    public void speedup(int increment){
+        System.out.println("applying speedup");
+    }
+}
+
+public class interface2 {
+    public static void main(String[] args) {
+
+    }
 }
