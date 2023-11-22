bbce5e1a39107c3bf43adb899f13a9eb45a71351
Ankit <ankitkrb4644@gmail.com>
2023-11-22 21:57:39 +0530

    update

--- interface1.java
+++ interface1.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class interface1 {
+interface Animal{
+    int eyes =2;
+    void walk();
+}
+interface Herbivore{
+
+}
+class Horse implements Animal ,Herbivore{
+    public void walk(){
+        System.out.println("walks on 4 legs");
+    }
+}
+public class interface1 {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+    }
 }
