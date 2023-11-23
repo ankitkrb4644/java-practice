55d2f2ae677b800f00caceceeae4164b83fd71a3
Ankit <ankitkrb4644@gmail.com>
2023-11-23 10:29:29 +0530

    update

--- Nested_Try_Catch.java
+++ Nested_Try_Catch.java
@@ -1,2 +1,21 @@
 public class Nested_Try_Catch {
+    public static void main(String[] args) {
+        int [] marks =  new int[3];
+        marks[0]=7;
+        marks[1]=56;
+        marks[2]=6;
+        try{
+            System.out.println("Welcome to video no 82");
+
+        try{
+            System.out.println(marks[9]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Sorry this index does not exits");
+        }
+    }
+    catch (Exception e){
+        System.out.println("Exception in level 1");
+        }
+    }
 }
