76c68f0ec6ebd7f823e28bacdf55888bd23d7e44
Ankit <ankitkrb4644@gmail.com>
2024-01-11 00:00:57 +0530

    update

--- variable.java
+++ variable.java
@@ -1,5 +1,10 @@
 public class variable {
     public static void main(String[] args) {
-    System.out.println("hii Ankit now we start dsa course ");
-    }
+    // System.out.println("hii Ankit now we start dsa course ");
+        int a =19;
+        int b =24;
+        int sum=a+b;
+        int diff = b-a;
+        System.out.println(diff);
+}
 }
