c664f53aee3288834fff94fd7b4d0a91701712b4
Ankit <ankitkrb4644@gmail.com>
2024-01-11 00:40:41 +0530

    update

--- variable.java
+++ variable.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class variable {
     public static void main(String[] args) {
     // System.out.println("hii Ankit now we start dsa course ");
-        int a =19;
-        int b =24;
-        int sum=a+b;
-        int diff = b-a;
-        System.out.println(diff);
-}
+        // variable
+        // int a =19;
+        // int b =24;
+        // int sum=a+b;
+        // int diff = b-a;
+        // System.out.println(diff);
+        // System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
 }
