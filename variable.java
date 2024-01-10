8059a4019de49b2b1acedfd680c9dd2dc9476e30
Ankit <ankitkrb4644@gmail.com>
2024-01-11 00:56:04 +0530

    update

--- variable.java
+++ variable.java
@@ -12,5 +12,11 @@ public class variable {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         System.out.println(name);
+        //second example
+        Scanner sc1 = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
     }
 }
