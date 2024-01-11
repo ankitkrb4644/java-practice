f92b636bb20b69c274b0c8d8cde09de02687af8d
Ankit <ankitkrb4644@gmail.com>
2024-01-12 00:15:43 +0530

    update

--- different_pattern_prob.java
+++ different_pattern_prob.java
@@ -1,8 +1,19 @@
 public class different_pattern_prob {
     public static void main(String[] args) {
         int n=4;
-        
+        int m=5;
+        // outer loop
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            // inner loop -> star print
+            // for(int j=1;j<=i;j++){
+            // System.out.print("*");
+            // }
+            System.out.println();
         }
-        }
+    }
+}
     
     
