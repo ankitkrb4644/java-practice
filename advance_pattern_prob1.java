c0de5948a214bd44173c2accc6bc85fd593e4f18
Ankit <ankitkrb4644@gmail.com>
2024-01-12 22:22:17 +0530

    final code

--- advance_pattern_prob1.java
+++ advance_pattern_prob1.java
@@ -4,15 +4,39 @@ public class advance_pattern_prob1{
         int n=5;
         //upper half part of butter fly
         for(int i=1;i<=n;i++){
+            // first half
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
-            System.out.println();
-        }
-        int spaces=2*(n-1);
+        // spaces
+        int spaces=2*(n-i);
         for(int j=1;j<=spaces;j++){
             System.out.print(" ");
         }
         // 2nd half part of butterfly
+            for(int j=1;j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        
+        }
+        // lower half
+        for(int i=n;i>=1;i--){
+            // first half
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+        // spaces
+        int spaces=2*(n-i);
+        for(int j=1;j<=spaces;j++){
+            System.out.print(" ");
+        }
+        // 2nd half part of butterfly
+            for(int j=1;j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        
+        }
     }
 }
