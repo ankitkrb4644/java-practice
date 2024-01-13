5d71cebc5f4175718fa8a6ce7e832426793466f0
Ankit <ankitkrb4644@gmail.com>
2024-01-13 21:59:14 +0530

    update

--- factorial.java
+++ factorial.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 public class factorial {
+    public static void printFactorial(int n) {
+        int factorial =1;
+        for(int i=n;i>=1;i--){
+            factorial = factorial * i;
+        }
+            return factorial ;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
