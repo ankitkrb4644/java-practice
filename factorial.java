90d24f1309aac7b0e7d2471a3c9758ae3e964b08
Ankit <ankitkrb4644@gmail.com>
2024-01-14 00:57:35 +0530

    update

--- factorial.java
+++ factorial.java
@@ -5,9 +5,9 @@ public class factorial {
         for(int i=n;i>=1;i--){
             factorial = factorial * i;
         }
-            return factorial ;
+            System.out.println(factorial);
     }
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
     }
