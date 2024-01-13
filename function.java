8c9164d5209af2fd19f4447dc5227c347038b7d6
Ankit <ankitkrb4644@gmail.com>
2024-01-13 18:38:12 +0530

    update

--- function.java
+++ function.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
 public class function {
-    
+    public static int calculateSum(int a , int b) {
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    }
 }
