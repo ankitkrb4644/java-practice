2e0252e329e194a2c1372947b82d919ad9e29446
Ankit <ankitkrb4644@gmail.com>
2024-01-13 20:33:51 +0530

    update

---
+++ factorial.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
