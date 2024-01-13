8bdd770272efdd088863ac6f1f64a2b60ad1ac67
Ankit <ankitkrb4644@gmail.com>
2024-01-13 19:18:26 +0530

    update

--- function.java
+++ function.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 public class function {
-    public static int calculateSum(int a , int b) {
-        int sum = a + b;
-        return sum;
+    // public static int calculateSum(int a , int b) {
+    //     int sum = a + b;
+    //     return sum;
+    // }
+    // public static void main(String args[]) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+        
+    //     int sum = calculateSum(a,b);
+    //     System.out.println("sum of 2 number is :" +sum);
+    public static int calculateMulti(int a , int b) {
+        int multi = a * b;
+        return multi;
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        
-        int sum = calculateSum(a,b);
-        System.out.println("sum of 2 number is :" +sum);
+        int multi = calculateMulti(a,b);
+        System.out.println("multiplication of 2 number is :" +multi);
     }
 }
